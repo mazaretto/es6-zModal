46e683a85ce9d09d512bc8a5ec6729b3d2eef175
mazaretto <i.dev@mazaretto.pro>
2019-01-12 00:45:37 +0700

    deploy v0.0.3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,1 @@
 import zModal from './zModal'
-
-let App = new zModal({
-    closeOnESC: true,
-    data: {
-        hello: 'World'
-    }
-}).createModal('helloworld', {
-    callbackFunction () {
-        console.log('Hello, ' + this.data.hello)
-    },
-    effect: 'fadein',
-    template: `<h1>Hello, {# hello #}</h1>`
-})
